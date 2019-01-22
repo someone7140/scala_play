b8c83288fc99d3296dc4cbf50adfaab135df1a31
someone7140 <ti7140@mail.goo.ne.jp>
2019-01-22 23:19:13 +0900

    修正

--- scala_slack/slack_test/src/main/scala/SlackTestMain.scala
+++ scala_slack/slack_test/src/main/scala/SlackTestMain.scala
@@ -5,24 +5,33 @@ import slack.api.SlackApiClient
 
 object Main extends App {
   implicit val system = ActorSystem("slack")
-  val token = "<Tokenを入力>"
+  val token = "ここに取得したtokenを設定"
   val client = SlackApiClient(token)
+
   // ユーザ一覧を取得するサンプル
   client.listUsers().onComplete {
     case Success(users) =>
       users.foreach(u =>
         println(s"${u.id}：${u.name}")
       )
-    case Failure(t) => println(s"${t.getMessage()}")
+    case Failure(t) => println(s"error:${t.getMessage()}")
   }
+
   // チャンネル一覧を取得するサンプル
   client.listChannels().onComplete {
     case Success(channels) =>
       channels.foreach(c =>
         println(s"${c.id}：${c.name}")
       )
-    case Failure(t) => println(s"${t.getMessage()}")
+    case Failure(t) => println(s"error:${t.getMessage()}")
   }
-  // チャネルIDと送信ユーザを指定してメッセージを送るサンプル
+
+  // チャネルIDまたは送信先のユーザIDを指定してtestメッセージを送るサンプル（送信者はToken発行ユーザ）
+  client.postChatMessage(channelId = "ここにIDを設定", text = "test", asUser = Some(true)).onComplete {
+    case Success(_) =>
+      println("Success")
+    case Failure(t) =>
+      println(s"error:${t.getMessage()}")
+  }
 
 }
